6c12d27a28d7b87efebce425dfe5324a7b12b3c2
Dong Wang <dong.d.wang@intel.com>
2023-06-19 14:52:28 +0800

    soc: add weak attribute to pm functions in sedi_soc.c
    they can be overwritten by real pm driver
    
    Signed-off-by: Dong Wang <dong.d.wang@intel.com>

--- bsp_sedi/soc/intel_ish/sedi_soc.c
+++ bsp_sedi/soc/intel_ish/sedi_soc.c
@@ -5,18 +5,19 @@
  */
 
 #include "sedi_driver_common.h"
-#include "sedi_driver_pm.h"
 
 /* weak PM functions used by SEDI drivers when SEDI PM driver is not enabled */
 
-void sedi_pm_set_device_power(IN sedi_devid_t id, IN sedi_power_state_t state)
+void __attribute__((weak)) sedi_pm_set_device_power(IN sedi_devid_t id,
+		IN sedi_power_state_t state)
 {
 	PARAM_UNUSED(id);
 	PARAM_UNUSED(state);
 }
 
-sedi_power_state_t sedi_pm_get_device_power(IN sedi_devid_t id)
+sedi_power_state_t __attribute__((weak)) sedi_pm_get_device_power(IN sedi_devid_t id)
 {
 	PARAM_UNUSED(id);
+
 	return SEDI_POWER_FULL;
 }
