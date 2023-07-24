64d44ce6d6901134fc26cdcba704daf95130f27b
Sun <xiaolu.sun@intel.com>
2023-07-24 12:04:36 +0800

    spi: start dma rx channel firstly
    - The SPI transaction may interrupted by some other events between Tx/Rx
      dma enable.
    - Rx dma is too late enabled so get SPI FIFO overflow and several data
      lost after the FIFO depth length, start Rx dma channel firstly can fix
      this issue.
    - Remaining problem is that spi didn't send rx overflow err bit interrupt.

--- bsp_sedi/drivers/spi/sedi_spi_dw_apb.c
+++ bsp_sedi/drivers/spi/sedi_spi_dw_apb.c
@@ -798,11 +798,11 @@ static void callback_dma_transfer(const sedi_dma_t dma, const int chan,
 						context->rx_channel, 0, 1,
 						lld_spi_dr_address(context->base),
 						(uint32_t)(context->data_rx), len, false);
-			/* Enable both channel to do transfer */
-			sedi_dma_start_transfer(context->tx_dma, context->tx_channel, (uint32_t)(context->data_tx),
-				lld_spi_dr_address(context->base), len);
+			/* Enable both channel and start rx firstly to do transfer */
 			sedi_dma_start_transfer(context->rx_dma, context->rx_channel, lld_spi_dr_address(context->base),
 						(uint32_t)(context->data_rx), len);
+			sedi_dma_start_transfer(context->tx_dma, context->tx_channel, (uint32_t)(context->data_tx),
+				lld_spi_dr_address(context->base), len);
 		}
 
 		/* Return to start another transfer */
@@ -970,10 +970,12 @@ int32_t sedi_spi_dma_transfer(IN sedi_spi_t spi_device, IN uint32_t tx_dma,
 		sedi_dma_start_transfer(rx_dma, rx_dma_chan, lld_spi_dr_address(context->base),
 					(uint32_t)data_in, len);
 	} else {
-		sedi_dma_start_transfer(tx_dma, tx_dma_chan, (uint32_t)data_out,
-			lld_spi_dr_address(context->base), len);
+		/* the SPI transaction may interrupted by some other events between Tx/Rx dma enable,
+		which probably lead to rx fifo overflow, start rx channel firstly */
 		sedi_dma_start_transfer(rx_dma, rx_dma_chan, lld_spi_dr_address(context->base),
 					(uint32_t)data_in, len);
+		sedi_dma_start_transfer(tx_dma, tx_dma_chan, (uint32_t)data_out,
+			lld_spi_dr_address(context->base), len);
 	}
 
 #endif
