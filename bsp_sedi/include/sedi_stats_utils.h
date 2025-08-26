845a8f7c1dd5642615404743e472e970d88a0080
Dong Wang <dong.d.wang@intel.com>
2025-08-26 11:29:48 +0800

    sedi_stats: Change to use BSD license
    Previously Intel license is used and checked in. Correct it.
    
    Signed-off-by: Dong Wang <dong.d.wang@intel.com>

--- bsp_sedi/include/sedi_stats_utils.h
+++ bsp_sedi/include/sedi_stats_utils.h
@@ -1,16 +1,7 @@
 /*
- * Copyright (c) 2023-2025 Intel Corporation. All Rights Reserved.
+ * Copyright (c) 2025 Intel Corporation
  *
- * This software and the related documents are Intel copyrighted materials,
- * and your use of them is governed by the express license under which they
- * were provided to you ("License"). Unless the License provides otherwise,
- * you may not use, modify, copy, publish, distribute, disclose or transmit
- * this software or the related documents without Intel's prior written
- * permission.
- *
- * This software and the related documents are provided as is, with no express
- * or implied warranties, other than those that are expressly stated in the
- * License.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 /* {sum, count, max, min, unit string} */
