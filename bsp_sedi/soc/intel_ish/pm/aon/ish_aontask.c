4731d8399e2d83eed3b350ac13b07c0fcf373ecf
Dong Wang <dong.d.wang@intel.com>
2026-08-28 11:06:12 +0800

    Revert "pm: allow reset when reset type is set"
    This reverts commit c46750677fb595924532ff7464fe4d9e7d31af4e.

--- bsp_sedi/soc/intel_ish/pm/aon/ish_aontask.c
+++ bsp_sedi/soc/intel_ish/pm/aon/ish_aontask.c
@@ -18,7 +18,6 @@
 #define AON_IDT_ENTRY_VEC_FIRST		SEDI_VEC_RESET_PREP
 #define AON_IDT_ENTRY_VEC_LAST		SEDI_VEC_PMU2IOAPIC
 
-#define PMU_RST_AP_REBOOT 0x40
 static void handle_reset(enum ish_pm_state pm_state);
 
 /* ISR for PMU wakeup interrupt */
@@ -653,8 +652,6 @@ static void sram_exit_sleep_mode(void)
 
 static void handle_d0i2(void)
 {
-	uint32_t reg_val;
-
 	ish_pg_exit_save_ctx();
 	aon_share.pg_exit = 0;
 
@@ -701,9 +698,7 @@ static void handle_d0i2(void)
 		continue;
 #endif
 
-	reg_val = read32(PMU_RST_PREP);
-	if ((reg_val & PMU_RST_PREP_AVAIL) ||
-		((reg_val & PMU_RST_PREP_RESET_TYPE) == PMU_RST_AP_REBOOT)) {
+	if (read32(PMU_RST_PREP) & PMU_RST_PREP_AVAIL) {
 		handle_reset(ISH_PM_STATE_RESET_PREP);
 	}
 
@@ -715,7 +710,6 @@ static void handle_d0i2(void)
 static void handle_d0i3(void)
 {
 	int ret;
-	uint32_t reg_val;
 
 	ish_pg_exit_save_ctx();
 	aon_share.pg_exit = 0;
@@ -749,9 +743,7 @@ static void handle_d0i3(void)
 
 	clear_vnnred_aoncg();
 
-	reg_val = read32(PMU_RST_PREP);
-	if ((reg_val & PMU_RST_PREP_AVAIL) ||
-		((reg_val & PMU_RST_PREP_RESET_TYPE) == PMU_RST_AP_REBOOT)) {
+	if (read32(PMU_RST_PREP) & PMU_RST_PREP_AVAIL) {
 		handle_reset(ISH_PM_STATE_RESET_PREP);
 	}
 
